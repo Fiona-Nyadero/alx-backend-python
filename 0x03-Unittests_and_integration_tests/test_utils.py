d7ec9a40f3fbb54381206dc60bdf3edbd96e5902
Fiona-Nyadero <fionanyadero@gmail.com>
2024-05-14 17:19:27 +0000

    Task 0 Corr2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 '''Module creates a parameterized unittest'''
 
 import unittest
-from parameterized import parameterized, parameterized.expand
+from parameterized import parameterized
 from typing import Any, Dict, Tuple
 from utils import access_nested_map
 
@@ -14,11 +14,15 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",)),
         ({"a": {"b": 2}}, ("a", "b")),
     ])
-    def test_access_nested_map(self, nested_map: Dict[str, Any], path: Tuple[str, ...], expected: Any) -> None:
+    def test_access_nested_map(self, nested_map: Dict[str, Any],
+                               path: Tuple[str, ...], expected: Any) -> None:
         '''Tests whether the method returns the correct value'''
         answer = access_nested_map(nested_map, path)
         self.AssertEqual(answer, expected)
 
 
 if __name__ = '__main__':
+    import unittest
+    from typing import cast
+
     unittest.main()
